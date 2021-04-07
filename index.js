487b64af601d0f79ab3aec1872f5b1d276fae11d
nangkossam <kr.dotware@gmail.com>
2021-04-07 21:12:08 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://nangko:nangko_db%232021@boilerplate.llazh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true
 }).then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!~안녕하세요~')
+  res.send('Hello World!~안녕하세요!!')
+})
+
+
+app.post('/register', (req, res) => {
+  // 회원 가입할 때 필요한 정보를 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+
+  const user = new User(req.body)
+  user.save((err, userInfo ) => {
+    if (err) return res.json({ success: false, err })
+
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
